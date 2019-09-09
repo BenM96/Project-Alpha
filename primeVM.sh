a1092863a87cf2c2c9bf18a80b916381b7b5c11b
Ubuntu <Ben@ran.jvr0ij04523eniebbeee5gkgoc.bx.internal.cloudapp.net>
2019-09-09 19:55:19 +0000

    Added file that gets a new vm ready by installing docker and docker-compose

---
+++ primeVM.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "priming vm with docker and docker-compose"
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -a -G docker $(whoami)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "finnished, restart shell to get full permisions"
