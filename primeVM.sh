d8256a535e8fb39a51fac02c6ef40c5c6a43a36b
BenM96 <49072028+BenM96@users.noreply.github.com>
2019-09-09 16:40:45 -0400

    Made dc pull sudo

--- primeVM.sh
+++ primeVM.sh
@@ -5,5 +5,5 @@ sudo apt-get install docker.io -y
 sudo usermod -a -G docker $(whoami)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose pull
+sudo docker-compose pull
 echo "finnished, restart shell to get full permisions"
